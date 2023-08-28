be493cbdf1d6ee287a02ae383ccfd8ae5553fbb3
renata-aline <renatalinec@gmail.com>
2023-08-28 12:53:11 -0300

    organizando o código

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -3,22 +3,22 @@ export class Negociacao {
   private _quantidade: number;
   private _valor: number;
 
-  constructor(data:Date, quantidade:number, valor:number) {
+  constructor(data: Date, quantidade: number, valor: number) {
     this._data = data;
     this._quantidade = quantidade;
     this._valor = valor;
   }
 
-  get data() {
+  get data(): Date {
     return this._data;
   }
-  get quantidade() {
+  get quantidade(): number {
     return this._quantidade;
   }
-  get valor() {
+  get valor(): number {
     return this._valor;
   }
-  get volume(){
+  get volume(): number {
     return this._quantidade * this._valor;
   }
 }
