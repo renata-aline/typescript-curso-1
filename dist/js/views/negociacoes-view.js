fb846adcb1ead5731f86035db80f32ff8dd04516
renata-aline <renatalinec@gmail.com>
2023-08-31 17:30:59 -0300

    integrando a view á página

---
+++ dist/js/views/negociacoes-view.js
@@ -0,0 +1,25 @@
+export class NegociacoesView {
+    constructor(seletor) {
+        this.elemento = document.querySelector(seletor);
+    }
+    template() {
+        return `
+        <table class="table table_hover table_bordered">
+            <thead>
+                <tr>
+                    <th>DATA</th>
+                    <th>QUANTIDADE</th>
+                    <th>VALOR</th>
+                
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
+            
+        `;
+    }
+    update() {
+        this.elemento.innerHTML = this.template();
+    }
+}
